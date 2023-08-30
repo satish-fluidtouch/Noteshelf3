70efe141cf2bff16140649c877e5295598e45557
Siva Kumar Reddy <sivakumar@fluidtouch.biz>
2023-08-30 10:44:11 +0530

    Tags functionality when performing from Notebook Center panel

--- Classes/Document UI/PDF Related/PDFRenderViewController/FTPDFRenderViewController+Commands.swift
+++ Classes/Document UI/PDF Related/PDFRenderViewController/FTPDFRenderViewController+Commands.swift
@@ -157,6 +157,7 @@ extension FTPDFRenderViewController: FTShortcutActions {
             page.bookmarkColor = "C69C3C"
         }
         (page as? FTNoteshelfPage)?.isDirty = true
+        NotificationCenter.default.post(name: .shouldReloadFinderNotification, object: nil)
     }
 
     func duplicateAction(pages: [FTThumbnailable], onCompletion: (()->())?) {
