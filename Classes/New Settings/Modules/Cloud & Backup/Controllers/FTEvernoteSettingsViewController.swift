48ce289a3302763ec2e2426267a85101dbe9e9a1
Rakesh P <rakesh@fluidtouch.com>
2023-09-01 17:04:09 +0530

    Compact Mode | Account View controller , Evernotecontroller cell height issue fixed.

--- Classes/New Settings/Modules/Cloud & Backup/Controllers/FTEvernoteSettingsViewController.swift
+++ Classes/New Settings/Modules/Cloud & Backup/Controllers/FTEvernoteSettingsViewController.swift
@@ -167,10 +167,6 @@ class FTEvernoteSettingsViewController: UIViewController, UITableViewDelegate, U
     }
 
     // MARK: - UITableViewDelegate
-     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-         return UITableView.automaticDimension
-    }
-    
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return (section == tableView.numberOfSections - 1) ? 76.0 : .leastNonzeroMagnitude
    }
