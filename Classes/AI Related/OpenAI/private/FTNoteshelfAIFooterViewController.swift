2ed2a1bef6b34a0ef6c6e28b6680b85933c46bfd
amarnathmn <amar@fluidtouch.biz>
2023-09-10 13:29:30 +0530

    made changes to avoid recognition in Ai if the user is non premium.

--- Classes/AI Related/OpenAI/private/FTNoteshelfAIFooterViewController.swift
+++ Classes/AI Related/OpenAI/private/FTNoteshelfAIFooterViewController.swift
@@ -14,11 +14,19 @@ enum FTAIFooterMode: Int {
 
 class FTNoteshelfAIFooterViewController: UIViewController {
     @IBOutlet private weak var learnMore: UIButton?;
+    @IBOutlet private weak var bePremiumUser: UIButton?;
     @IBOutlet private weak var sendFeedback: UIButton?;
 
     var footermode: FTAIFooterMode = .noteshelfAiBeta {
         didSet {
             self.learnMore?.isHidden = self.footermode == .sendFeedback
+            if self.footermode == .noteshelfAiBeta {
+                self.bePremiumUser?.isHidden = FTIAPManager.shared.premiumUser.isPremiumUser;
+            }
+            else {
+                self.bePremiumUser?.isHidden = true;
+            }
+            
             self.sendFeedback?.isHidden = self.footermode == .noteshelfAiBeta
         }
     }
@@ -29,12 +37,22 @@ class FTNoteshelfAIFooterViewController: UIViewController {
         
         let defaultFont = UIFont.systemFont(ofSize: 13);
         let attrTitle = NSAttributedString(string: "noteshelf.ai.noteshelfAILearnMore".aiLocalizedString, attributes: [.font: defaultFont]);
-//        let attrTitle = "noteshelf.ai.noteshelfAILearnMore".aiLocalizedString.appendBetalogo(font: defaultFont);
         
         self.learnMore?.setAttributedTitle(attrTitle, for: .normal);
         self.learnMore?.configuration?.imagePadding = 5;
 
-        self.sendFeedback?.setTitle("noteshelf.ai.sendFeedback".aiLocalizedString, for: .normal);
+//        let attributedTitle = NSAttributedString(string: "noteshelf.ai.handwriteMessage".aiLocalizedString,
+//                                                 attributes: [.font: UIFont.systemFont(ofSize: 13)])
+//        self.bePremiumUser?.setAttributedTitle(attributedTitle, for: .normal);
+        self.bePremiumUser?.configuration?.title = "noteshelf.ai.handwriteMessage".aiLocalizedString;
+//        self.bePremiumUser?.configuration?.baseForegroundColor = UIColor.appColor(.secondaryAccent);
+        self.bePremiumUser?.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        self.bePremiumUser?.titleLabel?.textAlignment = .center
+        self.bePremiumUser?.titleLabel?.minimumScaleFactor = 0.5;
+        
+        
+        self.sendFeedback?.configuration?.title = "noteshelf.ai.sendFeedback".aiLocalizedString;
+        self.sendFeedback?.titleLabel?.font = UIFont.systemFont(ofSize: 13)
     }
     
     @IBAction func showNoteshelfAILearnmore(_ sender: Any?) {
@@ -55,5 +73,9 @@ class FTNoteshelfAIFooterViewController: UIViewController {
         FTZenDeskManager.shared.showSupportContactUsScreen(controller: self.parent ?? self
                                                            , defaultSubject: "Noteshelf-AI Feedback"
                                                            , extraTags: ["Noteshelf-AI"]);
+    }
+    
+    @IBAction func bePremiumButtonAction(_ sender: Any?) {
+        FTIAPurchaseHelper.shared.presentIAPIfNeeded(on: self.parent ?? self);
     }
 }
