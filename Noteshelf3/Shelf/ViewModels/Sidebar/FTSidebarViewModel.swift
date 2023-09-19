e8b8e8d7c50c3aed971505059dd88eb1417d5984
Ramakrishna Gardas <ramakrishna@fluidtouch.biz>
2023-09-19 08:45:29 +0530

    Tech | Refactor of sidebar categories ordering code to maintain order in case of icloud on/off with keep on device option.

--- Noteshelf3/Shelf/ViewModels/Sidebar/FTSidebarViewModel.swift
+++ Noteshelf3/Shelf/ViewModels/Sidebar/FTSidebarViewModel.swift
@@ -656,30 +656,36 @@ extension FTSidebarViewModel {
         var bookmarksData: [FTCategoryBookmarkDataItem] = self.categoryBookmarksData.bookmarksData
         for sideBarItem in sidebarItems {
             if let collection = sideBarItem.shelfCollection {
-                if performURLResolving,let categoryBookmarkData = sidebarItemsBookmarksData.first(where: {$0.collectionURL == collection.URL}) {
-                    let collectionOrder = categoryBookmarkData.order
-                    orderedSideBarItems[sideBarItem] = collectionOrder
-                } else if !performURLResolving, let categoryBookmarkData = sidebarItemsBookmarksData.first(where: {$0.categoryName == collection.URL.lastPathComponent}) {
-                    let collectionOrder = categoryBookmarkData.order
+                if var existingCollectionInPlistIndex = bookmarksData.firstIndex(where:{$0.name == collection.URL.lastPathComponent}) {
+                    let existingCollectionInPlist = bookmarksData[existingCollectionInPlistIndex]
+                    let collectionOrder = existingCollectionInPlist.sortOrder
+                    if existingCollectionInPlist.fileURL != collection.URL, let newBookmarkItem = newCategoryBookmarkDataItemForCollection(collection, sortOrder: collectionOrder) { // updating url in already collection existing plist data
+                        bookmarksData.remove(at: existingCollectionInPlistIndex)
+                        bookmarksData.append(newBookmarkItem)
+                    }
                     orderedSideBarItems[sideBarItem] = collectionOrder
                 } else {
                     let maxOrderValue = bookmarksData.map({$0.sortOrder}).max() ?? 0
                     let newOrderValue = bookmarksData.isEmpty ? 0 : maxOrderValue + 1
-                    if let shelfItemCollection = sideBarItem.shelfCollection, let bookmarkData = URL.aliasData(shelfItemCollection.URL){
-                        let categoryBookmarkItem = FTCategoryBookmarkDataItem(bookmarkData: bookmarkData,
-                                                                              sortOrder: newOrderValue,
-                                                                              name: shelfItemCollection.URL.lastPathComponent,
-                                                                              fileURL: shelfItemCollection.URL)
-                        bookmarksData.append(categoryBookmarkItem)
+                    if let newBookmarkItem = newCategoryBookmarkDataItemForCollection(collection, sortOrder: newOrderValue){
+                        bookmarksData.append(newBookmarkItem)
+                        orderedSideBarItems[sideBarItem] = newOrderValue
                     }
-                    orderedSideBarItems[sideBarItem] = newOrderValue
                 }
             }
         }
         self.updateSidebarCategoriesOrderUsingDict(FTCategoryBookmarkData(bookmarksData: bookmarksData))
         return orderedSideBarItems.sorted(by: {$0.value < $1.value}).compactMap({$0.key})
     }
-
+    private func newCategoryBookmarkDataItemForCollection(_ collection: FTShelfItemCollection, sortOrder: Int) -> FTCategoryBookmarkDataItem? {
+        if let bookmarkData = URL.aliasData(collection.URL){
+            return FTCategoryBookmarkDataItem(bookmarkData: bookmarkData,
+                                       sortOrder: sortOrder,
+                                       name: collection.URL.lastPathComponent,
+                                       fileURL: collection.URL)
+        }
+        return nil
+    }
     func updateCategoryOrderBasedOnDeletedURLs(_ removedCategoryURLS:[URL]){
         removedCategoryURLS.forEach { url in
             if !FileManager.default.fileExists(atPath: url.path), let deletedSortInfoIndex = self.sidebarItemsBookmarksData.firstIndex(where: {$0.collectionURL?.urlByDeleteingPrivate().path == url.urlByDeleteingPrivate().path}) {
