aaaaba72a0636236f41beb9ca0fa3621b0e52cd1
Narayana <narayana@fluidtouch.biz>
2023-11-03 18:01:15 +0530

    Favorites | Favorite edit controller file

---
+++ Noteshelf3/Notebook/Controllers/Favoritebar/FTFavoriteEditViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FTFavoriteEditViewController.swift
+//  Noteshelf3
+//
+//  Created by Narayana on 03/11/23.
+//  Copyright © 2023 Fluid Touch Pte Ltd. All rights reserved.
+//
+
+import Foundation
+
+class FTFavoriteEditViewController: UIViewController {
+
+}
