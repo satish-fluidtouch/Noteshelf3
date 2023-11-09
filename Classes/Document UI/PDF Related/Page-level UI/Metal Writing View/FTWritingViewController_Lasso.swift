565927594ba420703467fad7cb7e366f4678257a
Amar Udupa <amar@fluidtouch.biz>
2023-11-09 15:27:08 +0530

    updated render-kit with a fix to getting image from offscreen rendered

--- Classes/Document UI/PDF Related/Page-level UI/Metal Writing View/FTWritingViewController_Lasso.swift
+++ Classes/Document UI/PDF Related/Page-level UI/Metal Writing View/FTWritingViewController_Lasso.swift
@@ -74,6 +74,7 @@ extension FTWritingViewController : FTLassoProtocol {
                             tileRect.origin.x = CGFloat(eachCol)*tileRect.width;
                             tileRect.origin.y = CGFloat(eachRow)*tileRect.height;
                             let request = FTOffScreenTileImageRequest(with: windowHash);
+                            request.imageAsCopy = false;
                             request.label = "LASSO_TILE_MOVE"
                             request.backgroundColor = bgColor
                             request.areaToRefresh = tileRect;
