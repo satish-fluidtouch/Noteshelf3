afa082d7a34b65410668050640d8d4fe81c79102
Sameer-fluidtouch <sameerhussain@fluidtouch.biz>
2023-11-20 16:37:43 +0530

    Setting image instead updating filewrapper

--- Classes/Document UI/PDF Related/Textbox/Text Input View/Bullets and Indentation/NSTextAttachment_Extension.swift
+++ Classes/Document UI/PDF Related/Textbox/Text Input View/Bullets and Indentation/NSTextAttachment_Extension.swift
@@ -17,27 +17,19 @@ extension NSTextAttachment {
                 fileWrapper.preferredFilename = "Attachment.png";
                 self.fileWrapper = fileWrapper;
             }
-        } else {
-            if migrate {
-                let contents = self.fileWrapper?.regularFileContents
-                let ns2CheckBoxOffAttachmentData = UIImage(named: "check-off-2x_ns2.png")?.pngData()
-                let ns2CheckBoxonAttachmentData = UIImage(named: "check-on-2x_ns2.png")?.pngData()
-                guard let checkBoxOffContents = ns2CheckBoxOffAttachmentData,
-                    let checkBoxOnContents = ns2CheckBoxonAttachmentData
-                    else {
-                        return;
-                }
-                let isSameAsCheckOff = (contents == checkBoxOffContents);
-                let isSameAsCheckOn = (contents == checkBoxOnContents);
-
-                if isSameAsCheckOff || isSameAsCheckOn {
-                    let image = isSameAsCheckOff ? UIImage(named: "check-off-2x.png") : UIImage(named: "check-on-2x.png")
-                    if let image, let imageData = image.pngData() {
-                        let fileWrapper = FileWrapper.init(regularFileWithContents: imageData);
-                        fileWrapper.preferredFilename = "Attachment.png";
-                        self.fileWrapper = fileWrapper;
-                    }
-                }
+        }
+        if migrate {
+            let contents = self.fileWrapper?.regularFileContents
+            let ns2CheckBoxOffAttachmentData = UIImage(named: "check-off-2x_ns2.png")?.pngData()
+            let ns2CheckBoxonAttachmentData = UIImage(named: "check-on-2x_ns2.png")?.pngData()
+            guard let checkBoxOffContents = ns2CheckBoxOffAttachmentData,
+                  let checkBoxOnContents = ns2CheckBoxonAttachmentData else {
+                return;
+            }
+            let isSameAsCheckOff = (contents == checkBoxOffContents);
+            let isSameAsCheckOn = (contents == checkBoxOnContents);
+            if isSameAsCheckOff || isSameAsCheckOn {
+                self.image = isSameAsCheckOff ? UIImage(named: "check-off-2x.png") : UIImage(named: "check-on-2x.png")
             }
         }
     }
