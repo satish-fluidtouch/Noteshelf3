03f9d41f26e61366ddde89a1056dcfbe45b76e4a
Sameer-fluidtouch <sameerhussain@fluidtouch.biz>
2023-11-20 16:24:07 +0530

    migrating checkbox textlist to match with ns3 design

--- Classes/Document UI/PDF Related/Textbox/Text Input View/Bullets and Indentation/NSTextAttachment_Extension.swift
+++ Classes/Document UI/PDF Related/Textbox/Text Input View/Bullets and Indentation/NSTextAttachment_Extension.swift
@@ -9,13 +9,35 @@
 import UIKit
 
 extension NSTextAttachment {
-  func updateFileWrapperIfNeeded()
+    func updateFileWrapperIfNeeded(migrate: Bool = false)
     {
         if(nil == self.fileWrapper) {
-            if let image = self.image,let imageData = image.pngData() {
+            if let image = self.image, let imageData = image.pngData() {
                 let fileWrapper = FileWrapper.init(regularFileWithContents: imageData);
                 fileWrapper.preferredFilename = "Attachment.png";
                 self.fileWrapper = fileWrapper;
+            }
+        } else {
+            if migrate {
+                let contents = self.fileWrapper?.regularFileContents
+                let ns2CheckBoxOffAttachmentData = UIImage(named: "check-off-2x_ns2.png")?.pngData()
+                let ns2CheckBoxonAttachmentData = UIImage(named: "check-on-2x_ns2.png")?.pngData()
+                guard let checkBoxOffContents = ns2CheckBoxOffAttachmentData,
+                    let checkBoxOnContents = ns2CheckBoxonAttachmentData
+                    else {
+                        return;
+                }
+                let isSameAsCheckOff = (contents == checkBoxOffContents);
+                let isSameAsCheckOn = (contents == checkBoxOnContents);
+
+                if isSameAsCheckOff || isSameAsCheckOn {
+                    let image = isSameAsCheckOff ? UIImage(named: "check-off-2x.png") : UIImage(named: "check-on-2x.png")
+                    if let image, let imageData = image.pngData() {
+                        let fileWrapper = FileWrapper.init(regularFileWithContents: imageData);
+                        fileWrapper.preferredFilename = "Attachment.png";
+                        self.fileWrapper = fileWrapper;
+                    }
+                }
             }
         }
     }
