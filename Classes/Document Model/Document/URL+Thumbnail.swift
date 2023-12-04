2c752f3f9b35f577c9f9115179a4ed206379432c
Akshay <akshay@fluidtouch.biz>
2023-12-04 16:05:52 +0530

    Revert "Fixed crash due to NSLog with path()"
    This reverts commit df03fb8cac1804bc225b32f28af7d5242771a9fc.

--- Classes/Document Model/Document/URL+Thumbnail.swift
+++ Classes/Document Model/Document/URL+Thumbnail.swift
@@ -17,29 +17,14 @@ extension URL {
                                                    size: CGSize(width: 300, height: 300),
                                                    scale: 2,
                                                    representationTypes: .thumbnail)
-#if DEBUG
-        NSLog("🌄 Requesting thumbnail for \(self.path)")
-#endif
-
-        NSLog("called: \(self.path())");
         let generator = QLThumbnailGenerator.shared
         generator.generateRepresentations(for: request) { thumbnail, _, error in
             DispatchQueue.main.async {
-                NSLog("called respinde: \(self.path())");
                 if let thumbnail {
-#if DEBUG
-                    NSLog("🌄 Thumbnail Fetched for \(self)")
-#endif
                     completion(thumbnail.uiImage,error)
                 } else if let error {
-#if DEBUG
-                    NSLog("🌄 Thumbnail Error \(error)")
-#endif
                     completion(nil,error)
                 } else {
-#if DEBUG
-                    NSLog("🌄 Thumbnail Unknown Error")
-#endif
                     completion(nil,error)
                 }
             }
