991f0565acc4ad68631c5ae562138c55071775e7
Narayana <narayana@fluidtouch.biz>
2024-01-22 14:29:02 +0530

    Block has been changed to inner function, log error added

--- Classes/Document UI/PDF Related/AnnotationControllers/FTTextAnnotationViewController.swift
+++ Classes/Document UI/PDF Related/AnnotationControllers/FTTextAnnotationViewController.swift
@@ -298,28 +298,26 @@ class FTTextAnnotationViewController: UIViewController {
             track("textmode_page_tapped", params: ["postit_color" : "clear"], screenName: FTScreenNames.textbox)
         }
         
-        let updatetTextConfigBlock = { [weak self] in
-            //update default properties
-            guard let self else { return }
+        func updateTextConfig() {
             if let defaultFont = page?.parentDocument?.localMetadataCache?.defaultBodyFont {
                 self.textInputView.setValue(defaultFont,
-                                       forAttribute: NSAttributedString.Key.font.rawValue,
-                                       in: NSRange(location: 0, length: 0))
+                                            forAttribute: NSAttributedString.Key.font.rawValue,
+                                            in: NSRange(location: 0, length: 0))
             }
             if let defaultColor = page?.parentDocument?.localMetadataCache?.defaultTextColor {
                 self.textInputView.setValue(defaultColor,
-                                       forAttribute: NSAttributedString.Key.foregroundColor.rawValue,
-                                       in: NSRange(location: 0, length: 0))
+                                            forAttribute: NSAttributedString.Key.foregroundColor.rawValue,
+                                            in: NSRange(location: 0, length: 0))
             }
             if let underlineValue = page?.parentDocument?.localMetadataCache?.defaultIsUnderline {
                 self.textInputView.setValue(NSNumber.init(value: underlineValue),
-                                       forAttribute: NSAttributedString.Key.underlineStyle.rawValue,
-                                       in: NSRange(location: 0, length: 0))
+                                            forAttribute: NSAttributedString.Key.underlineStyle.rawValue,
+                                            in: NSRange(location: 0, length: 0))
             }
             if let strikeThrough = page?.parentDocument?.localMetadataCache?.defaultIsStrikeThrough {
                 self.textInputView.setValue(NSNumber.init(value: strikeThrough),
-                                       forAttribute: NSAttributedString.Key.strikethroughStyle.rawValue,
-                                       in: NSRange(location: 0, length: 0))
+                                            forAttribute: NSAttributedString.Key.strikethroughStyle.rawValue,
+                                            in: NSRange(location: 0, length: 0))
             }
             if let alignment = page?.parentDocument?.localMetadataCache?.defaultTextAlignment, let lineSpace = page?.parentDocument?.localMetadataCache?.defaultAutoLineSpace  {
                 let paragraphStyle = NSMutableParagraphStyle()
@@ -333,18 +331,19 @@ class FTTextAnnotationViewController: UIViewController {
             }
             textView.scale = self.zoomScale
 
-        #if targetEnvironment(macCatalyst)
+#if targetEnvironment(macCatalyst)
             self.delegate?.annotationControllerWillBeginEditing?(self)
-        #endif
+#endif
             self.delegate?.annotationControllerDidAdded(self)
         }
 
         if !Thread.isMainThread {
+            FTLogError("TextInput", attributes: ["Thread": Thread.current])
             runInMainThread {
-                updatetTextConfigBlock()
+                updateTextConfig()
             }
         } else {
-            updatetTextConfigBlock()
+            updateTextConfig()
         }
     }
     
