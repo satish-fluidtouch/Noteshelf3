726f8a5671adea8bcf8fe372eea6d65861754449
Sameer-fluidtouch <sameerhussain@fluidtouch.biz>
2024-02-21 18:18:16 +0530

    Saving the document once page is inserted

--- Classes/Document UI/PDF Related/Split Mode/Controllers/FTDocumentRenderViewController.swift
+++ Classes/Document UI/PDF Related/Split Mode/Controllers/FTDocumentRenderViewController.swift
@@ -339,29 +339,31 @@ extension FTDocumentRenderViewController: FTDocumentViewPresenter {
             guard let insertedPage = doc.insertPageBelow(page: refPage) else {
                return
             }
-            self.documentViewController.showPage(at: insertedPage.pageIndex(), forceReLayout: true)
-            switch type {
-            case .pen:
-                self.documentViewController.updateToolBar(with: RKDeskMode.deskModePen)
-                break;
-            case .audio:
-                self.documentViewController.audioButtonAction()
-                break;
-            case .openAI:
-//                self.documentViewController.switch(RKDeskMode.des)
-                self.documentViewController.firstPageController()?.startOpenAiForPage()
-                break;
-            case .text:
-                self.documentViewController.updateToolBar(with: RKDeskMode.deskModeText)
-                let info = FTTextAnnotationInfo();
-                info.localmetadataCache = insertedPage.parentDocument?.localMetadataCache;
-                info.visibleRect = self.documentViewController.firstPageController()?.scrollView?.visibleRect() ?? self.view.frame
-                info.atPoint = self.contentHolderView?.center ?? .zero;
-                info.scale = self.documentViewController.contentScaleInNormalMode;
-                self.documentViewController.firstPageController()?.addAnnotation(info: info)
-                break;
-            case .bookOpen(_):
-                break
+            doc.saveDocument { _ in
+                self.documentViewController.showPage(at: insertedPage.pageIndex(), forceReLayout: true)
+                switch type {
+                case .pen:
+                    self.documentViewController.updateToolBar(with: RKDeskMode.deskModePen)
+                    break;
+                case .audio:
+                    self.documentViewController.audioButtonAction()
+                    break;
+                case .openAI:
+                    //                self.documentViewController.switch(RKDeskMode.des)
+                    self.documentViewController.firstPageController()?.startOpenAiForPage()
+                    break;
+                case .text:
+                    self.documentViewController.updateToolBar(with: RKDeskMode.deskModeText)
+                    let info = FTTextAnnotationInfo();
+                    info.localmetadataCache = insertedPage.parentDocument?.localMetadataCache;
+                    info.visibleRect = self.documentViewController.firstPageController()?.scrollView?.visibleRect() ?? self.view.frame
+                    info.atPoint = self.contentHolderView?.center ?? .zero;
+                    info.scale = self.documentViewController.contentScaleInNormalMode;
+                    self.documentViewController.firstPageController()?.addAnnotation(info: info)
+                    break;
+                case .bookOpen(_):
+                    break
+                }
             }
         }
         
