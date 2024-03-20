07fbf3bb985ad0c5ac69e0fcd7ed8ce961ed077b
Narayana <narayana@fluidtouch.biz>
2024-03-20 14:28:55 +0530

    Watch | OS10 sample complication

--- Noteshelf3 Watch Widget/AppIntent.swift
+++ Noteshelf3 Watch Widget/AppIntent.swift
@@ -10,10 +10,16 @@ import WidgetKit
 import AppIntents
 
 struct ConfigurationAppIntent: WidgetConfigurationIntent {
+//struct ConfigurationAppIntent: AppIntent {
     static var title: LocalizedStringResource = "Configuration"
     static var description = IntentDescription("This is an example widget.")
 
     // An example configurable parameter.
     @Parameter(title: "Favorite Emoji", default: "😃")
     var favoriteEmoji: String
+
+//    func perform() async throws -> some IntentResult {
+//        print("zzzz - ConfigurationAppIntent ")
+//        return .result()
+//    }
 }
--- Noteshelf3 Watch Widget/Noteshelf3_Watch_WidgetsBundle.swift
+++ Noteshelf3 Watch Widget/Noteshelf3_Watch_WidgetsBundle.swift
@@ -12,26 +12,80 @@ import SwiftUI
 @main
 struct Noteshelf3_Watch_WidgetsBundle: WidgetBundle {
     var body: some Widget {
-        CaffeineComplication()
+        FTWatchComplication()
     }
 }
 
-
-struct CaffeineComplication: Widget {
-    let kind: String = "Caffeine-Complication"
+struct FTWatchComplication: Widget {
+    let kind: String = "FT-Complication"
 
     var body: some WidgetConfiguration {
         AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
-            ComplcationSampleView(entry: entry)
+            FTComplicationView(entry: entry)
                 .containerBackground(.fill.tertiary, for: .widget)
         }
     }
+
+//        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+//            FTComplicationView(entry: entry)
+//        }
+//        .configurationDisplayName("NS3 Opener")
+//        .description("Opens Noteshelf3 app")
+//        .supportedFamilies([.accessoryCircular, .accessoryCorner, .accessoryInline, .accessoryRectangular])
+//    }
 }
 
-struct ComplcationSampleView: View {
+struct FTComplicationView: View {
+    var entry: Provider.Entry
+    @Environment(\.widgetFamily) var widgetFamily
+
+    var body: some View {
+        switch widgetFamily {
+        case .accessoryCorner:
+            FTComplicationCorner(entry: entry)
+        case .accessoryCircular:
+            FTComplicationCircular(entry: entry)
+        case .accessoryInline:
+            FTComplicationInline(entry: entry)
+        case .accessoryRectangular:
+            FTComplicationRectangular(entry: entry)
+        @unknown default:
+            //mandatory as there are more widget families as in lockscreen widgets etc
+            Text("Not an implemented widget yet")
+        }
+    }
+
+}
+
+struct FTComplicationInline : View {
     var entry: Provider.Entry
 
     var body: some View {
-        Text("In Progress")
+        Text(entry.date.formatted())
     }
 }
+
+struct FTComplicationCircular : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Image("watch_complication")
+    }
+}
+
+struct FTComplicationCorner : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Image("watch_complication")
+    }
+}
+
+struct FTComplicationRectangular : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Image("watch_complication")
+    }
+}
+
