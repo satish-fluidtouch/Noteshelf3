ee3a8c3814e004b2bcba1ec719230b2c1effe316
Narayana <narayana@fluidtouch.biz>
2024-04-12 19:17:19 +0530

    Premium banner UI changes, localization

--- Noteshelf3/IAPurchase/FTPremiumBanner.swift
+++ Noteshelf3/IAPurchase/FTPremiumBanner.swift
@@ -42,19 +42,27 @@ struct FTPremiumBanner: View {
                         .multilineTextAlignment(.leading)
                         .font(.appFont(for: .regular, with: 13))
                         .foregroundColor(Color.appColor(.accent))
-                        .padding(.horizontal, 8)
                         .padding(.vertical, 2.5)
-                        .background(Color.appColor(.accent).opacity(0.1))
                         .clipShape(RoundedRectangle(cornerRadius: 6))
 
                     Spacer()
                         .frame(height: 12)
 
-                    Text("premiumBanner.featureInfo".localized)
-                        .foregroundColor(Color.appColor(.black70))
-                        .font(.appFont(for: .regular, with: 13))
-                        .multilineTextAlignment(.leading)
-                        .fixedSize(horizontal: false, vertical: true)
+                    Button {
+                        viewModel.delegate?.didTapOnUpgradeNow();
+                    } label: {
+                        Text("iap.upgradeNow".localized)
+                            .font(.appFont(for: .bold, with: 13))
+                            .foregroundColor(Color.white)
+                            .padding(.horizontal, 12)
+                            .padding(.vertical, 3)
+                            .frame(height: 28,alignment: .leading)
+                            .background(Color("premium_bg"))
+                            .cornerRadius(6)
+                            .shadow(color: .black.opacity(0.04), radius: 0.5, x: 0, y: 3)
+                            .shadow(color: .black.opacity(0.12), radius: 4, x: 0, y: 3)
+                    }
+                    .buttonStyle(FTMicroInteractionButtonStyle(scaleValue: .slow))
                 }
                 .padding(.top, 0)
                 .padding(.bottom, 16)
