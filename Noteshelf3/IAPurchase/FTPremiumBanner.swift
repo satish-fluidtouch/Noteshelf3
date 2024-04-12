72ff640d7d8324c1b353a3476e0f63c894e1cffa
Narayana <narayana@fluidtouch.biz>
2024-04-12 17:56:08 +0530

    Shelf | Premium banner. changes

--- Noteshelf3/IAPurchase/FTPremiumBanner.swift
+++ Noteshelf3/IAPurchase/FTPremiumBanner.swift
@@ -13,55 +13,88 @@ import FTCommon
 struct FTPremiumBanner: View {
     @EnvironmentObject var viewModel : FTSidebarViewModel
     @EnvironmentObject var premiumUser : FTPremiumUser
+    @State var height: CGFloat = 136
 
     var body: some View {
         ZStack {
-            HStack(alignment: .center, spacing: 10) {
-                Image("premium_icon", bundle: nil)
-                    .frame(width: 36, height: 36, alignment: .center)
-                    .scaledToFit()
+            HStack(spacing: 0) {
+                VStack {
+                    Image("premium_icon", bundle: nil)
+                        .frame(width: 36, height: 36, alignment: .center)
+                        .scaledToFit()
 
-                VStack(alignment: .leading, spacing: 4) {
+                    Spacer()
+
+                    Image("premium_banner_wave")
+                        .scaledToFill()
+                        .frame(width: 76, height: 76, alignment: .bottomLeading)
+                        .foregroundStyle(Color(hex: "0455CF", alpha: 0.2))
+                }
+
+                VStack(alignment: .leading, spacing: 0) {
                     Text("iap.bannerTitle".localized)
                         .font(.appFont(for: .bold, with: 15))
                         .multilineTextAlignment(.leading)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .padding(.bottom, 4)
+
                     Text(String(format: "iap.booksleft".localized, NSNumber(value: max(premiumUser.maxBookLimitForFree - premiumUser.numberOfBookCreate,0))))
                         .multilineTextAlignment(.leading)
                         .font(.appFont(for: .regular, with: 13))
                         .foregroundColor(Color.appColor(.accent))
+                        .padding(.horizontal, 8)
+                        .padding(.vertical, 2.5)
+                        .background(Color.appColor(.accent).opacity(0.1))
+                        .clipShape(RoundedRectangle(cornerRadius: 6))
 
                     Spacer()
+                        .frame(height: 12)
 
-                    Button {
-                        viewModel.delegate?.didTapOnUpgradeNow();
-                    } label: {
-                        Text("iap.upgradeNow".localized)
-                            .font(.appFont(for: .bold, with: 13))
-                            .foregroundColor(Color.white)
-                            .padding(.horizontal, 12)
-                            .padding(.vertical, 3)
-                            .frame(height: 28,alignment: .leading)
-                            .background(Color("premium_bg"))
-                            .cornerRadius(6)
-                            .shadow(color: .black.opacity(0.04), radius: 0.5, x: 0, y: 3)
-                            .shadow(color: .black.opacity(0.12), radius: 4, x: 0, y: 3)
-                    }
-                    .buttonStyle(FTMicroInteractionButtonStyle(scaleValue: .slow))
+                    Text("premiumBanner.featureInfo".localized)
+                        .foregroundColor(Color.appColor(.black70))
+                        .font(.appFont(for: .regular, with: 13))
+                        .multilineTextAlignment(.leading)
+                        .fixedSize(horizontal: false, vertical: true)
                 }
+                .padding(.top, 0)
+                .padding(.bottom, 16)
+                .padding(.trailing, 20)
                 Spacer()
             }
-            .frame(maxWidth: .infinity)
-            .padding(EdgeInsets(top: 16, leading: 20, bottom: 16, trailing: 4))
+            .padding(.top, 16)
         }
-        .frame(height:108)
-        .background(Color.appColor(.white50))
-        .background(.ultraThinMaterial)
+        .background(
+            GeometryReader { geometry in
+                Color.appColor(.premiumBannerBgColor)
+                    .background(.ultraThinMaterial)
+                    .preference(key: HeightPreferenceKey.self, value: geometry.size.height)
+            })
         .cornerRadius(12)
-        .shadow(color: .black.opacity(0.04), radius: 6, x: 0, y: 4)
+        .shadow(color: .black.opacity(0.04), radius: 12, x: 0, y: 4)
         .overlay(
-          RoundedRectangle(cornerRadius: 12)
-            .inset(by: 0.25)
-            .stroke(Color.appColor(.toolbarOutline), lineWidth: 0.5)
+            RoundedRectangle(cornerRadius: 12)
+                .inset(by: 0.25)
+                .stroke(Color.appColor(.toolbarOutline), lineWidth: 0.5)
         )
+        .frame(height: height)
+        .onPreferenceChange(HeightPreferenceKey.self) {
+            self.height = $0
+        }
+        .onTapGesture {
+            viewModel.delegate?.didTapOnUpgradeNow()
+        }
+    }
+}
+
+private struct HeightPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
+
+struct FTPremiumBanner_Previews: PreviewProvider {
+    static var previews: some View {
+        FTPremiumBanner()
     }
 }
